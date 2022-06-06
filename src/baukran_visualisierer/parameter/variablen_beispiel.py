0597b9f8a5c2f3d8edb9a7117544e0df38fc2c52
Johannes Gerwert <jogerwert@gmail.com>
2022-06-06 23:05:27 +0200

    Fehlermeldung "LogicError" fuer Fehler in der Logik erstellt; Modellierung Baufeld als 3D-Dict; es gibt nur noch einen Kran; Anfaenge fuer Modellierung der Anweisungen; Modellierung Kran verbessert; Beispiel-Baustelle erstellt

---
+++ src/baukran_visualisierer/parameter/variablen_beispiel.py
@@ -0,0 +1,22 @@
+
+baustelle_name = "Beispielbaustelle"
+
+# (laenge, breite)
+baufeld_tupel = (10, 10)
+
+# (x, y, hoehe, ausladung)
+kran_tupel = (0, 0, 0, 5, 10)
+
+# (x, y, z)
+gegenstand_1_tupel = (6, 2, 0)
+gegenstand_2_tupel = (4, 4, 0)
+
+# (x, y, z)
+bauteil_1_tupel = ("Bauteil1", 7, 6, 0)
+bauteil_2_tupel = ("Bauteil2", 7, 7, 0)
+bauteil_3_tupel = ("Bauteil3", 7, 8, 0)
+
+# (x, y, z)
+bauteil_4_tupel = ("Bauteil4", 6, 6, 0)
+bauteil_5_tupel = ("Bauteil5", 6, 6, 1)
+bauteil_6_tupel = ("Bauteil6", 6, 6, 2)
