bb58b13d82e68225c3a83d4fe180d943fa79a895
Johannes Gerwert <jogerwert@gmail.com>
2022-07-18 19:14:22 +0200

    Anpassung Parser Mockup an Beispiel; Berechnung Kranposition aus Koordinaten; Berechnung Koordinaten aus Kranposition

--- src/baukran_visualisierer/model/kran.py
+++ src/baukran_visualisierer/model/kran.py
@@ -1,47 +1,53 @@
 import math
 
+from src.baukran_visualisierer.exceptions.logic_error import LogicError
 from src.baukran_visualisierer.model.bauteil import Bauteil
 
 
 class Kran:
 
-    def __init__(self, position_x, position_y, position_z, hoehe, ausladung):
+    def __init__(self, position_x, position_y, hoehe, ausladung):
         self.position_x = position_x
         self.position_y = position_y
-        self.position_z = position_z
+        self.position_z = 0
         self.hoehe = hoehe
-        self.gesamt_hoehe = self.position_z + self.hoehe
         self.ausladung = ausladung
 
         # Haken ganz oben
-        self.haken_hoehe = self.gesamt_hoehe
+        self.haken_hoehe = self.hoehe
 
         # Laufkatze direkt am Mast
         self.laufkatze_entfernung = 1
 
-        # Ausleger in Richtung x-Achse
+        # Ausleger zeigt in Richtung x-Achse
         self.winkel = 0
+
+        # Kran steht in der Mitte des Kaestchens, nicht auf der Ecke
+        self.raster_versatz = 0.5
 
         self.baustelle = None
         self.haken_anhaengsel = None
 
     def greife(self):
         if self.baustelle is None:
-            raise RuntimeError("Kran wurde keiner Baustelle zugewiesen!")
+            raise LogicError("Kran wurde keiner Baustelle zugewiesen!")
 
         if self.haken_anhaengsel is not None:
-            raise RuntimeError("An dem Haken haengt bereits ein Bauteil!")
+            raise LogicError("An dem Haken haengt bereits ein Bauteil!")
 
-        # TODO: Positionsberechnung testen
-        haken_x = self.laufkatze_entfernung * math.cos(math.radians(self.winkel))
-        haken_y = self.laufkatze_entfernung * math.sin(math.radians(self.winkel))
-        haken_z = self.haken_hoehe
+        haken_koordinaten_tuple = self.berechne_koordinaten_aus_kranposition(
+            self.laufkatze_entfernung, self.winkel, self.haken_hoehe)
+
+        haken_x = haken_koordinaten_tuple[0]
+        haken_y = haken_koordinaten_tuple[1]
+        haken_z = haken_koordinaten_tuple[2]
+
         haken_anhaengsel = self.baustelle.erhalte_inhalt_koordinate(haken_x, haken_y, haken_z)
 
         if isinstance(haken_anhaengsel, Bauteil):
             self.haken_anhaengsel = haken_anhaengsel
         else:
-            raise RuntimeError(f'An der Koordinate ({haken_x}, {haken_y}, {haken_z}) befindet sich kein Bauteil!')
+            raise LogicError(f'An der Koordinate ({haken_x}, {haken_y}, {haken_z}) befindet sich kein Bauteil!')
 
     def richte_aus(self):
         pass
@@ -64,3 +70,42 @@ class Kran:
         # TODO: pruefe ob positiv
         # TODO: pruefe ob hoehe kran ueberschritten
         self.haken_hoehe = self.haken_hoehe + hoehe
+
+    def berechne_koordinaten_aus_kranposition(self, laufkatze_entfernung, winkel, haken_hoehe):
+        # TODO: testen
+
+        # Berechnung: Ankathete im Dreieck aus Winkel und Hypotenuse + Versatz
+        haken_x = math.floor(
+            self.position_x + self.raster_versatz
+            + laufkatze_entfernung * math.cos(math.radians(winkel))
+        )
+
+        # Berechnung: Gegenkathete im Dreieck aus Winkel und Hypotenuse + Versatz
+        haken_y = math.floor(
+            self.position_y + self.raster_versatz
+            + laufkatze_entfernung * math.sin(math.radians(winkel))
+        )
+
+        haken_z = math.floor(haken_hoehe)
+
+        return haken_x, haken_y, haken_z
+
+    def berechne_kranposition_aus_koordinaten(self, haken_x, haken_y, haken_z):
+        # TODO: testen
+
+        # Berechnung: Abstand zwischen zwei Punkten, Rasterversatz kuerzt sich raus
+        laufkatze_entfernung = math.sqrt(
+            math.pow(self.position_x - haken_x, 2)
+            + math.pow(self.position_y - haken_y, 2)
+        )
+
+        if laufkatze_entfernung > self.ausladung:
+            raise LogicError(f'Das Bauteil an Position ({haken_x}, {haken_y}, {haken_z}) '
+                             f'liegt ausserhalb der Reichweite des Krans!')
+
+        # Berechnung: Winkel im Dreieck aus Hypotenuse und Ankathete
+        winkel = math.degrees(math.acos(haken_x / laufkatze_entfernung))
+
+        haken_hoehe = haken_z
+
+        return laufkatze_entfernung, winkel, haken_hoehe
--- src/baukran_visualisierer/service/parser_service.py
+++ src/baukran_visualisierer/service/parser_service.py
@@ -4,24 +4,24 @@ from src.baukran_visualisierer.model.bauteil import Bauteil
 from src.baukran_visualisierer.model.gegenstand import Gegenstand
 from src.baukran_visualisierer.model.kran import Kran
 
-import src.baukran_visualisierer.parameter.variablen_beispiel as var
-
 
 def erstelle_beispiel_baustelle():
-    name = var.baustelle_name
-    baufeld = Baufeld(var.baufeld_tupel[0], var.baufeld_tupel[1])
-    kran = Kran(var.kran_tupel[0], var.kran_tupel[1], var.kran_tupel[2], var.kran_tupel[3], var.kran_tupel[4])
+    name = "HTWSaar"
+    # (laenge, breite)
+    baufeld = Baufeld(10, 5)
+    # (x, y, hoehe, ausladung)
+    kran = Kran(0, 0, 5, 10)
 
-    gegenstaende = [Gegenstand(var.gegenstand_1_tupel[0], var.gegenstand_1_tupel[1], var.gegenstand_1_tupel[2]),
-                    Gegenstand(var.gegenstand_2_tupel[0], var.gegenstand_2_tupel[1], var.gegenstand_2_tupel[2])]
+    # (x, y, z)
+    gegenstaende = [Gegenstand(3, 2, 0),
+                    Gegenstand(3, 2, 1)]
 
-    bauteile = [Bauteil(var.bauteil_1_tupel[0], var.bauteil_1_tupel[1], var.bauteil_1_tupel[2], var.bauteil_1_tupel[3]),
-                Bauteil(var.bauteil_2_tupel[0], var.bauteil_2_tupel[1], var.bauteil_2_tupel[2], var.bauteil_2_tupel[3]),
-                Bauteil(var.bauteil_3_tupel[0], var.bauteil_3_tupel[1], var.bauteil_3_tupel[2], var.bauteil_3_tupel[3]),
-                Bauteil(var.bauteil_4_tupel[0], var.bauteil_4_tupel[1], var.bauteil_4_tupel[2], var.bauteil_4_tupel[3]),
-                Bauteil(var.bauteil_5_tupel[0], var.bauteil_5_tupel[1], var.bauteil_5_tupel[2], var.bauteil_5_tupel[3]),
-                Bauteil(var.bauteil_6_tupel[0], var.bauteil_6_tupel[1], var.bauteil_6_tupel[2], var.bauteil_6_tupel[3])]
+    # (x, y, z)
+    bauteile = [Bauteil("BasisLinks", 0, 2, 0),
+                Bauteil("BasisRechts", 1, 2, 0),
+                Bauteil("Dach", 2, 2, 0)]
 
+    # TODO: Montageanweisungen
     montageanweisungen = []
 
     baustelle = Baustelle(name, baufeld, kran, gegenstaende, bauteile, montageanweisungen)
