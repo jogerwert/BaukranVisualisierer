7ed9e0e504607950e046e1e6034e6fa211fdf7df
peter <peterpham11@yahoo.de>
2022-09-23 00:00:09 +0200

    Refaktorisierung
    Erhöhung der Lesbarkeit des Codes

--- src/grafischer_prototyp/GrafikBaufeld.py
+++ src/grafischer_prototyp/GrafikBaufeld.py
@@ -5,6 +5,14 @@ class GrafikBaufeld:
         self.liste = []
 
     def erzeuge_baufeld(self, eingabe_laenge_baufeld, eingabe_breite_baufeld):
+        """
+            Notes:
+            -Diese Funktion erzeugt das Baufeld. Es wird zwischen zwei Farben gewechselt wie bei einem Schachbrett.
+            -Zeile 19,21,24,26 wird die Farbe des Baufeldes entschieden. vector nimmt Werte von 0 bis 1 and wobei 1 den
+            Wert 255 in RGB color code entspricht. Eine direkte RGB color code ist in dem benutzen Framework nicht
+            möglich
+        """
+
         laenge_baufeldteil = breite_baufeldteil = 1
         hoehe_baufeldteil = 0.1
         for x in range(0, eingabe_laenge_baufeld):
@@ -19,9 +27,6 @@ class GrafikBaufeld:
                         color_baufeld = vector(0.9, 0.9, 0.9)
                     else:
                         color_baufeld = vector(0.8, 0.8, 0.8)
-
-                #box(pos=vector(x, -0.5, y), length=laenge_baufeldteil, width=breite_baufeldteil, height=hoehe_baufeldteil,
-                #    color=color_baufeld)
 
                 self.liste.append(box(pos=vector(x, -0.5, y), length=laenge_baufeldteil, width=breite_baufeldteil,
                                       height=hoehe_baufeldteil,color=color_baufeld))
--- src/grafischer_prototyp/GrafikObjekte.py
+++ src/grafischer_prototyp/GrafikObjekte.py
@@ -1,6 +1,10 @@
 from vpython import *
 
 class GrafikBauteil:
+    """
+        Notes:
+        Diese Klasse erzeugt ein für den Nutzer sichtbares Bauteil, was vom Kran bewegt werden kann.
+    """
     def erzeuge_bauteil(self,name, eingabe_position_wert_x, eingabe_position_wert_y, eingabe_position_wert_z):
         bauteil_position = vector(eingabe_position_wert_x, eingabe_position_wert_z, eingabe_position_wert_y)
         self.name = name
@@ -13,6 +17,10 @@ class GrafikBauteil:
         return self.bauteil
 
 class GrafikHindernis:
+    """
+        Notes:
+        Diese Klasse erzeugt ein für den Nutzer sichtbares Hindernis, was vom Kran nicht bewegt werden soll.
+    """
     def erzeuge_hindernis(self, eingabe_position_wert_x, eingabe_position_wert_y, eingabe_position_wert_z):
         bauteil_position = vector(eingabe_position_wert_x, eingabe_position_wert_z, eingabe_position_wert_y)
         self.hindernis = box(pos=bauteil_position, color=vector(1, 0, 1), opacity=0.8)
@@ -24,6 +32,11 @@ class GrafikHindernis:
         return self.hindernis
 
 class GrafikPosition:
+    """
+        Notes:
+        Diese Klasse erzeugt ein unsichtbares Positionsobjekt, welches an Kranbefehle übergeben werden kann,
+        damit diese Befehle wissen, wohin Ausleger,Greifarm und Laufkatze sich hinbewegen sollen.
+    """
     def erzeuge_position(self, eingabe_position_wert_x, eingabe_position_wert_y, eingabe_position_wert_z):
         self.position = vector(eingabe_position_wert_x, eingabe_position_wert_z, eingabe_position_wert_y)
 
