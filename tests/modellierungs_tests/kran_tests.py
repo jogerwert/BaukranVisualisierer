19030aa6c585b15c5f39eb655b57ca4d17e85019
Johannes Gerwert <jogerwert@gmail.com>
2022-09-23 18:41:05 +0200

    Löschen unnötiger Test; Verbesserung Name des Installers

--- tests/modellierungs_tests/kran_tests.py
+++
@@ -1,7 +0,0 @@
-from tests.modellierungs_tests import beispiel_baustellen
-
-
-def test():
-    baustelle = beispiel_baustellen.erstelle_beispiel_baustelle()
-    kran = baustelle.kran
-    kran.bringe_an(1, 2, 3)
